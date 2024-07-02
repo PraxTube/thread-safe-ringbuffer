cce32db060eb29d596a7ddf6c3650e12d1652f9f
Rancic <lukinator28@gmail.com>
2024-07-02 20:34:28 +0200

    ci: Run threaded test multiple times in CI

---
+++ multi-threaded-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+test_executable=./build/test_threaded/test
+
+for i in $(seq 1 100); do
+  if [ ! -f $test_executable ]; then
+    echo "echo -e \"\033[0;31mFILE NOT FOUND\033[0m --- $test_executable\"" >> $summary_file
+  elif [ -x "$test_executable" ]; then
+    echo -e "\n===> \033[0;34mRUNNING\033[0m $test_executable, NUM: $i <===\n"
+    timeout 5s "$test_executable"
+    if [ $? -eq 0 ]; then
+			echo -e "\n\033[0;32mPASSED\033[0m"
+    else
+			echo -e "\n\033[0;31mFAILED\033[0m"
+      exit 1
+    fi
+  fi
+done
